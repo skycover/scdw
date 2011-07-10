db54ea6a42a717c160eb1ca4e6c2c0e45c8a5d1e
Dmitry Chernyak <dmi.chernyak@gmail.com>
2011-07-10 14:37:11 +0400

    Added i18n notes

--- settings.py
+++ settings.py
@@ -35,6 +35,7 @@ TIME_ZONE = 'Europe/Moscow'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
+#LANGUAGE_CODE = 'en-us'
 LANGUAGE_CODE = 'ru-RU'
 
 SITE_ID = 1
