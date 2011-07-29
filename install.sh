16c897c199863a955976d7e3c2632a63df664b1f
Dmitry Chernyak <dmi.chernyak@gmail.com>
2011-07-29 15:02:07 +0400

    Improved install.sh
    * The whole code is now copied to the directory named 'scdw'
    * the binary path, install path and port now specified by -b -d and -p
    options

--- install.sh
+++ install.sh
@@ -1,18 +1,41 @@
 #!/bin/sh
-./manage.py syncdb
+set -e
 name=scdw
+prefix=/usr/local
 port=8088
-if [ -z "$1" ]; then
-  target=/usr/local/bin
-elif [ -d "$1" ]; then
-  target="$1"
-else
-  echo "Usage $0 'install-path' [port number]"
+target=/usr/local/bin
+usage() {
+  echo "Usage $0 [-b /path/to/scdw-binary (/usr/local/bin)] [-p port-number (8088)] [-d /path/to/scdw-directory (/usr/local)]"
   exit 1
-fi
-if [ -n "$2" ]; then
-  port=$2
-fi
+}
+while [ -n "$1" ]; do
+  case "$1" in
+    "-b")
+      test -n "$2" || usage
+      target="$2"
+      shift 2
+    ;;
+    "-p")
+      test -n "$2" || usage
+      port="$2"
+      shift 2
+    ;;
+    "-d")
+      test -n "$2" || usage
+      prefix="$2"
+      shift 2
+    ;;
+    *)
+      usage
+    ;;
+  esac
+done
+mkdir -p $target
+dstdir=$prefix/$name
+mkdir -p $dstdir
+cp -r * $dstdir
+cd $dstdir
+./manage.py syncdb
 cat <<EOF >"$target"/$name
 #!/bin/sh
 port=$port
@@ -23,6 +46,6 @@ exec `pwd`/manage.py runserver \$port
 EOF
 chmod +x "$target"/$name
 cat <<EOF
-SkyCover Duply Web is installed as $target/$name
+SkyCover Duply Web binary is installed as $target/$name
 By default it will listen on the port http://localhost:$port
 EOF
